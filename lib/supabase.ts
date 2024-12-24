2184e6dcd6a9c589cde4c56090f23795286382fb
Daniel <vinokuda@gmail.com>
2024-12-24 11:51:02 -0500

    Refactor Supabase configuration to use environment variables for better security and flexibility
    - Replaced hardcoded Supabase URL and anon key with environment variables (EXPO_PUBLIC_SUPABASE_URL and EXPO_PUBLIC_SUPABASE_ANON_KEY).
    - This change enhances security by preventing sensitive information from being exposed in the codebase.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://soiarviiyysgaomrnvlv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvaWFydmlpeXlzZ2FvbXJudmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4ODQ0NzAsImV4cCI6MjA0OTQ2MDQ3MH0.IlRX43nSRwvYlJMX7g5hy7Mi48PH7eN_TQpdci6LN-E'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
